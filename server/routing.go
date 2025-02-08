bf71587790b0fdf91f12610f87a81f78cfdb53d4
adam.xu <adam.xu@mantle.xyz>
2025-02-08 18:01:45 +0800

    fix return HTTP status code 200 when using a malformat commitment

--- server/routing.go
+++ server/routing.go
@@ -20,7 +20,7 @@ func (svr *Server) registerRoutes(r *mux.Router) {
 	subrouterGET.HandleFunc("/"+
 		"{optional_prefix:(?:0x)?}"+ // commitments can be prefixed with 0x
 		"{"+routingVarNameVersionByteHex+":[0-9a-fA-F]{2}}"+ // should always be 0x00 for now but we let others through to return a 404
-		"{"+routingVarNamePayloadHex+":[0-9a-fA-F]*}",
+		"{"+routingVarNamePayloadHex+":(?:[0-9a-fA-F]{2})*}",
 		withLogging(withMetrics(svr.handleGetStdCommitment, svr.m, commitments.Standard), svr.log),
 	).Queries("commitment_mode", "standard")
 	// op keccak256 commitments (write to S3)
@@ -30,7 +30,7 @@ func (svr *Server) registerRoutes(r *mux.Router) {
 		// we don't use version_byte for keccak commitments, because not expecting keccak commitments to change,
 		// but perhaps we should (in case we want a v2 to use another hash for eg?)
 		// "{version_byte_hex:[0-9a-fA-F]{2}}"+ // should always be 0x00 for now but we let others through to return a 404
-		"{"+routingVarNamePayloadHex+"}",
+		"{"+routingVarNamePayloadHex+":(?:[0-9a-fA-F]{2})*}",
 		withLogging(withMetrics(svr.handleGetOPKeccakCommitment, svr.m, commitments.OptimismKeccak), svr.log),
 	)
 	// op generic commitments (write to EigenDA)
@@ -39,7 +39,7 @@ func (svr *Server) registerRoutes(r *mux.Router) {
 		"{"+routingVarNameCommitTypeByteHex+":01}"+ // 01 for generic commitments
 		"{da_layer_byte:[0-9a-fA-F]{2}}"+ // should always be 0x00 for eigenDA but we let others through to return a 404
 		"{"+routingVarNameVersionByteHex+":[0-9a-fA-F]{2}}"+ // should always be 0x00 for now but we let others through to return a 404
-		"{"+routingVarNamePayloadHex+"}",
+		"{"+routingVarNamePayloadHex+":(?:[0-9a-fA-F]{2})*}",
 		withLogging(withMetrics(svr.handleGetOPGenericCommitment, svr.m, commitments.OptimismGeneric), svr.log),
 	)
 	// unrecognized op commitment type (not 00 or 01)
@@ -60,7 +60,7 @@ func (svr *Server) registerRoutes(r *mux.Router) {
 		"{"+routingVarNameCommitTypeByteHex+":01}"+ // 01 for generic commitments
 		"{da_layer_byte:[0-9a-fA-F]{2}}"+ // should always be 0x00 for eigenDA but we let others through to return a 404
 		"{"+routingVarNameVersionByteHex+":[0-9a-fA-F]{2}}"+ // should always be 0x00 for now but we let others through to return a 404
-		"{"+routingVarNamePayloadHex+"}",
+		"{"+routingVarNamePayloadHex+":(?:[0-9a-fA-F]{2})*}",
 		withLogging(withMetrics(svr.HandleGetExtra, svr.m, commitments.OptimismGeneric), svr.log),
 	)
 
--- server/server_extra.go
+++ server/server_extra.go
@@ -16,36 +16,46 @@ import (
 
 // HandleGet handles the GET_EXTRA request for commitments.
 func (svr *Server) HandleGetExtra(w http.ResponseWriter, r *http.Request) error {
+	extraInfo, err := svr.getExtra(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return nil
+	}
+
+	svr.writeResponse(w, extraInfo)
+	return err
+}
+
+func (svr *Server) getExtra(r *http.Request) ([]byte, error) {
 	rawCommitmentHex, ok := mux.Vars(r)[routingVarNamePayloadHex]
 	if !ok {
-		return fmt.Errorf("commitment not found in path: %s", r.URL.Path)
+		return nil, fmt.Errorf("commitment not found in path: %s", r.URL.Path)
 	}
 	commitment, err := hex.DecodeString(rawCommitmentHex)
 	if err != nil {
-		return fmt.Errorf("failed to decode rawCommitmentHex %s: %w", rawCommitmentHex, err)
+		return nil, fmt.Errorf("failed to decode rawCommitmentHex %s: %w", rawCommitmentHex, err)
 	}
 
 	var cert verify.Certificate
 	err = rlp.DecodeBytes(commitment, &cert)
 	if err != nil {
-		return fmt.Errorf("failed to decode DA cert to RLP format: %w", err)
+		return nil, fmt.Errorf("failed to decode DA cert to RLP format: %w", err)
 	}
 
 	sm, ok := svr.sm.(*store.Manager)
 	if !ok {
-		return errors.New("store manager get extra info is unsupported")
+		return nil, errors.New("store manager get extra info is unsupported")
 	}
 
 	client, ok := sm.GetEigenDAClient().(*eigenda.EigenDAClientProxy)
 	if !ok {
-		return errors.New("client get extra info is unsupported")
+		return nil, errors.New("client get extra info is unsupported")
 	}
 
 	extraInfo, err := client.GetBlobExtra(r.Context(), cert.BlobVerificationProof.BatchMetadata.BatchHeaderHash, cert.BlobVerificationProof.BlobIndex)
 	if err == nil {
-		svr.writeResponse(w, extraInfo)
-		return nil
+		return extraInfo, nil
 	}
 
-	return fmt.Errorf("failed to get extra info for commitment %v", rawCommitmentHex)
+	return nil, fmt.Errorf("failed to get extra info for commitment %v", rawCommitmentHex)
 }
